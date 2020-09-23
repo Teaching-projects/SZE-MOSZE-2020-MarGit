6c323eb634b9f8e9f04d41f8465a2ec68a3d5641
erikmeszaros <me.erik1995@gmail.com>
2020-09-23 11:42:57 +0200

    Take out IO and some small changes

--- Character.cpp
+++ Character.cpp
@@ -1,10 +1,7 @@
 #include "Character.h"
 #include <iostream>
-
-//constructor
 Character::Character(std::string char_name,  int char_hp, int char_dmg ) : name(char_name),hp(char_hp), damage(char_dmg) {
 }
-//getters
 std::string Character::getName() const {
 	return name;
 }
@@ -14,24 +11,18 @@ int Character::getHp() const {
 int Character::getDamage() const{
 	return damage;
 }
-//func
 void Character::attack(Character&  target) const{
-	if (target.getHp() < this->getDamage()){
+	if (target.getHp() < getDamage()){
 		target.hp=0;
 	}
 	else{
-		target.hp=target.getHp()-this->getDamage();	
+		target.hp-=getDamage();	
 	}
-}
-//for stat_out
-std::ostream& operator<<(std::ostream& os, const Character* const character)
-{
-    os << character->getName() << "\t HP: " << character->getHp() << " , DMG: " << character->getDamage() <<"\n";
-    return os;
-}
-void Character::stat_out () const{
-	std::cout << this;
 }
 bool Character::isDead() const{
 	return getHp()==0;
 }
+std::ostream& operator<<(std::ostream& os, const Character& character){
+    os << character.getName() << "\t HP: " << character.getHp() << " , DMG: " << character.getDamage() <<"\n";
+    return os;
+}	
--- Character.h
+++ Character.h
@@ -3,26 +3,21 @@
 #ifndef CHARACTER_CLASS_H
 #define CHARACTER_CLASS_H
 
-
 class Character
 {
 private:
-	int hp; const int  damage;
+	int hp;
+	const int  damage;
 	const std::string name;
 
 public:
-	//constructor
 	Character(std::string char_name, int char_hp, int char_dmg);
-	//getters
 	std::string getName() const;
 	int getHp() const;
 	int getDamage() const;
-	//func
 	void attack(Character& target) const;
 	friend std::ostream& operator<<(std::ostream& os, const Character& character);
-	void stat_out() const;
 	bool isDead() const;
 };
-
-
+std::ostream& operator<<(std::ostream& os, const Character& character);
 #endif
