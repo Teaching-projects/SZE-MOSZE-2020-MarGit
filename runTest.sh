1720e809c76b50aa71d5e9ab6d17d5b4bc5fd53c
erikmeszaros <me.erik1995@gmail.com>
2020-09-25 23:00:07 +0200

    Add result.txt az runTest shell script

---
+++ runTest.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rm output.txt
+touch > output.txt
+
+a.out Units/Unit1.json Units/Unit2.json >> output.txt
+a.out Units/Unit1.json Units/Unit3.json >> output.txt
+a.out Units/Unit2.json Units/Unit1.json >> output.txt
+a.out Units/Unit2.json Units/Unit3.json >> output.txt
+a.out Units/Unit3.json Units/Unit1.json >> output.txt
+a.out Units/Unit3.json Units/Unit2.json >> output.txt
+
+if cmp -s output.txt result.txt
+then
+    exit 0
+else
+    exit 1
+fi
+
